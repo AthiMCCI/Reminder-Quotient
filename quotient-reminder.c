4ed67bd6aa27a56c66ec7d0241a12fa4092078e0
“Athisankar” <athisankar@mcci.com>
2022-05-16 17:40:12 +0530

    remove change

--- quotient-reminder.c
+++ quotient-reminder.c
@@ -2,12 +2,16 @@
 int main()
 {
     int div,divisor,quot,rem;
+
     printf("enter div");
     scanf("%d",&div);
+
     printf("enter divisor");
     scanf("%d",&divisor);
+
     quot = div/divisor;
     rem = div % divisor;
+
     printf("quot=%d \n", quot);
     printf("rem=%d",rem);
 }
