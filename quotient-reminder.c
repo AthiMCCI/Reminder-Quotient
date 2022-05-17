356e3fcad98485a63e765557c987d3da7ff41e7a
“Athisankar” <athisankar@mcci.com>
2022-05-17 13:58:56 +0530

    #3: upload coding standard

--- quotient-reminder.c
+++ quotient-reminder.c
@@ -1,17 +1,53 @@
-#include<stdio.h>
-int main()
-{
-    int div,divisor,quot,rem;
+/*
+Module: Compute-quotient-and-reminder.c
 
-    printf("enter div");
+Function:
+    Computing quotient and reminder of a given number.
+
+Copyright notice:
+    This file copyright (C) 2022 by
+    MCCI Corporation
+    3520 Krums Corners Road
+    Ithaca, NY 14850
+    An unpublished work. All rights reserved.
+    This file is proprietary information, and may not be disclosed or
+    copied without the prior permission of MCCI Corporation.
+
+Author:
+        Athisankar, MCCI Corporation   May 2022
+*/
+
+#include <stdio.h>
+
+/*********************************************\
+|
+|   Code.
+|
+\*********************************************/
+
+int main()
+    {
+    printf("\n");
+    printf("*********************************************\n");
+    printf("THIS IS QUOTIENT and REMINDER v1.0.0\n");
+    printf("*********************************************\n");
+    printf("\n");
+    printf("IT IS USED TO FIND QUOTIENT AND REMINDER \n \n");
+
+    int div, divisor, quot, rem;
+
+    printf("ENTER DIV");
     scanf("%d",&div);
 
-    printf("enter divisor");
+    printf("enter DIVISOR");
     scanf("%d",&divisor);
 
     quot = div/divisor;
     rem = div % divisor;
 
+    printf("\n \n******************************************\n");
     printf("quot=%d \n", quot);
     printf("rem=%d",rem);
-}
+
+    return 0;
+    }
